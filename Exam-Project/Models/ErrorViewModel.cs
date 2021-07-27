4d8e2ce5705d026f65b09bcf5937bee62002d5f4
kirilvankov <kirilevankov@gmail.com>
2021-07-27 10:57:59 +0300

    Add project files.

---
+++ Exam-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Exam_Project.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
