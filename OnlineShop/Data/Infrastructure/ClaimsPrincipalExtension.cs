7645cce5595cfb30cf4877869252e5be0f9b5621
kirilvankov <kirilevankov@gmail.com>
2022-10-29 01:11:28 +0300

    Create Store apply form.

--- OnlineShop/Data/Infrastructure/ClaimsPrincipalExtension.cs
+++ OnlineShop/Data/Infrastructure/ClaimsPrincipalExtension.cs
@@ -7,5 +7,7 @@
     {
         public static string GetId(this ClaimsPrincipal user)
             => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        public static string GetEmail(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.Email).Value;
     }
 }
---
+++ OnlineShop/Models/AddressInfoViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace OnlineShop.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddressInfoViewModel
+    {
+        [Required]
+        [Display(Name = "Address")]
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string PostCode { get; set; }
+
+        [RegularExpression("^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$", ErrorMessage = "Latitude is invaluid")]
+        public double? LocationLat { get; set; }
+
+        [RegularExpression("^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$", ErrorMessage = "Longitude is invalid")]
+        public double? LocationLng { get; set; }
+    }
+}
